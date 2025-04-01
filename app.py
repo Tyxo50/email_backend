9d92c96700ef17c09443a7bbfbc4ccdfa8d49503
TaMLeNok <t.lelievre718@gmail.com>
2025-04-01 11:48:33 -0400

    fix for onrender

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def send_email():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
